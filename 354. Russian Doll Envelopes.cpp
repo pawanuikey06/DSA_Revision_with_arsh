8a69134c902d479b9547d2e90d0406f7bcfa61b2
Pawan Kumar Uikey <130145010+pawanuikey06@users.noreply.github.com>
2025-01-02 23:28:11 +0530

    Dynamic Programming +Binary Search

--- 354. Russian Doll Envelopes.cpp
+++ 354. Russian Doll Envelopes.cpp
@@ -23,3 +23,51 @@ public:
         return finalCount;
     }
 };
+
+// Dynamic Programming TLE
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end());
+        if (envelopes.size() == 1)
+            return 1;
+        vector<int>dp(envelopes.size(),1);
+        int count=1;
+        for(int i=0;i<envelopes.size();i++){
+
+            for(int j=0;j<i;j++){
+                 if(envelopes[i][0]>envelopes[j][0] && envelopes[i][1]>envelopes[j][1]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                    count =max(count,dp[i]);
+
+                 }
+            }
+        }
+        return count;
+    }
+};
+
+//  Dynamic Programming + Lower_bound
+class Solution {
+public:
+   
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(),[](vector<int>&a,vector<int>&b){
+        if(a[0]==b[0]){
+            return a[1]>b[1];
+        }
+        return a[0]<b[0];
+    });
+       vector<int>lis;
+       for(auto &vec :envelopes){
+            auto it =lower_bound(begin(lis),end(lis),vec[1]);
+
+            if(it==lis.end()){
+                lis.push_back(vec[1]);
+            }else{
+                *it=vec[1];
+            }
+       }
+       return lis.size();
+    }
+};
