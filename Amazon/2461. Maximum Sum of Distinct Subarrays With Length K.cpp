72524ca147d238afff12152c34881cb7fd197b02
Pawan Kumar Uikey <130145010+pawanuikey06@users.noreply.github.com>
2025-01-06 19:39:32 +0530

    Map +sliding window

--- Amazon/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
+++ Amazon/2461. Maximum Sum of Distinct Subarrays With Length K.cpp
@@ -3,7 +3,6 @@ public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         long long n = nums.size(), ws = 0, mx = 0;
         unordered_map<int, int> mp;
-
         for (int i = 0; i < n; i++) {
             // Add the current element to the window
             mp[nums[i]]++;
